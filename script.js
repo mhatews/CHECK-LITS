fa941609a6f8fc829bcf13d88fae926c21718ccc
Matheus P. Oliveira <mat.p.oliveira@gmail.com>
2024-10-02 13:29:52 -0300

    02 - 10 : 1° Commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const boxModels = [
 
     { id: 'adesivo7x10Box', label: 'Adesivos 7X10 ' },
+    { id: 'adesivo5x6Box', label: 'Adesivos 5X6 ' },
     { id: 'adesivo10x10Box', label: 'Adesivos 10X10 ' },
     { id: '8ladosBox', label: 'Caixas 8 Lados' },
     { id: 'alcaBox', label: 'Caixas Alça' },
